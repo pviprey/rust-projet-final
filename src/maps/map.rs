fd27155bf45b933dee5edb655e0222d5e403b3cf
Flamby <66632621+Kaeggame@users.noreply.github.com>
2025-03-17 23:04:12 +0100

    Iron added and robot can collect it
    - Iron on map.
    - Robot can collect Iron by walking on it.
    - Iron is collected on the robot and not for all. (for futur use)

--- src/maps/map.rs
+++ src/maps/map.rs
@@ -10,8 +10,10 @@ pub fn generate_map() {
     for x in 0..20 {
         for y in 0..100 {
             let noise = perlin.get([x as f64 / 10.0, y as f64 / 10.0, 0.0]);
-            if noise < 0.0 {
+            if noise < -0.3 {
                 print!("#");
+            } else if noise < -0.1 {
+                print!("F");
             } else {
                 print!(".");
             }
@@ -27,17 +29,36 @@ pub fn generate_map_with_robot() {
     let map_height = 100;
     let mut robot = Robot::new(map_width, map_height);
     
+    let mut iron_collected = vec![vec![false; map_height]; map_width];
+    
     loop {
         print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
         
+        let noise_at_robot = perlin.get([robot.x as f64 / 10.0, robot.y as f64 / 10.0, 0.0]);
+        if noise_at_robot < -0.1 && noise_at_robot >= -0.3 && !iron_collected[robot.x][robot.y] {
+            robot.collect_iron();
+            iron_collected[robot.x][robot.y] = true;
+        }
+        
+        // status
+        println!("Iron Collected: {}", robot.iron_collected);
+        println!();
+        
+        // map
         for x in 0..map_width {
             for y in 0..map_height {
                 if robot.x == x && robot.y == y {
                     print!("R");
                 } else {
                     let noise = perlin.get([x as f64 / 10.0, y as f64 / 10.0, 0.0]);
-                    if noise < 0.0 {
+                    if noise < -0.3 {
                         print!("#");
+                    } else if noise < -0.1 {
+                        if !iron_collected[x][y] {
+                            print!("F");
+                        } else {
+                            print!(".");
+                        }
                     } else {
                         print!(".");
                     }
--- src/robots/robot.rs
+++ src/robots/robot.rs
@@ -6,6 +6,7 @@ pub struct Robot {
     map_width: usize,
     map_height: usize,
     energy: i32,
+    pub iron_collected: i32,
 }
 
 impl Robot {
@@ -15,12 +16,16 @@ impl Robot {
             y: map_height / 2,
             map_width,
             map_height,
-            energy: 100
+            energy: 100,
+            iron_collected: 0,
         }
     }
     
+    pub fn collect_iron(&mut self) {
+        self.iron_collected += 1;
+    }
+    
     pub fn move_random(&mut self) {
-
         if self.energy <= 0 {
             return;
         } else {
@@ -29,10 +34,10 @@ impl Robot {
         let mut rng = rand::rng();
         // 0: haut, 1: droite, 2: bas, 3: gauche
         match rng.random_range(0..4) {
-            0 if self.y > 0 => self.y -= 1,
-            1 if self.x < self.map_width - 1 => self.x += 1,
-            2 if self.y < self.map_height - 1 => self.y += 1,
-            3 if self.x > 0 => self.x -= 1,
+        0 if self.y > 0 => self.y -= 1,
+        1 if self.x < self.map_width - 1 => self.x += 1,
+        2 if self.y < self.map_height - 1 => self.y += 1,
+        3 if self.x > 0 => self.x -= 1,
             _ => {},
         }
     }
